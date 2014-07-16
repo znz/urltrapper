51737147980ab73d6fe42b9f8e9a2f0c9255c7a4
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-16 21:06:21 +0900

    connect buttons and actions

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,7 @@
 class AppDelegate
   def initialize
     super
-    #setEventHandler
+    setEventHandler
   end
 
   def applicationDidFinishLaunching(notification)
@@ -16,58 +16,6 @@ class AppDelegate
   def buildWindow
     @mainWindowController = MainWindow.alloc.initWithWindowNibName('MainWindow')
     @mainWindowController.window.makeKeyAndOrderFront(self)
-    return
-
-    width  = 480
-    height = 360
-    @mainWindow = NSWindow.alloc.initWithContentRect([[240, 180], [width, height]],
-      styleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask,
-      backing: NSBackingStoreBuffered,
-      defer: false)
-    @mainWindow.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
-    @mainWindow.orderFrontRegardless
-
-    margin = 10
-    text_height = height/2
-    @text = NSTextField.alloc.initWithFrame([[margin, height-text_height], [width-margin*2, text_height-margin]])
-    @text.autoresizingMask = NSViewMinXMargin|NSViewMinYMargin|NSViewWidthSizable|NSViewHeightSizable
-    @text.stringValue = @url || "http://www.apple.com/" # default URL
-    @mainWindow.contentView.addSubview(@text)
-
-    buildButtons(width, height, margin)
-  end
-
-  MY_DEBUG = false
-
-  BUTTONS = [
-    [
-      'Set Default Browser',
-      :setDefaultBrowser,
-    ],
-    [
-      'Open Firefox',
-      :openFirefox,
-    ],
-    [
-      'Open Google Chrome',
-      :openChrome,
-    ],
-    [
-      'Open Safari',
-      :openSafari,
-    ],
-  ]
-
-  def buildButtons(width, height, margin)
-    BUTTONS.reverse_each.with_index do |(title, action), idx|
-      button = NSButton.alloc.initWithFrame([[margin, margin+30*idx], [width-margin*2, 32]])
-      button.title = title
-      button.action = action
-      button.target = self
-      button.bezelStyle = NSRoundedBezelStyle
-      button.autoresizingMask = NSViewMinXMargin|NSViewMaxYMargin|NSViewWidthSizable
-      @mainWindow.contentView.addSubview(button)
-    end
   end
 
   def setEventHandler
@@ -78,54 +26,11 @@ class AppDelegate
       andEventID: KAEGetURL)
   end
 
-  def setDefaultBrowser
-    bundle_id = NSBundle.mainBundle.bundleIdentifier
-    LSSetDefaultHandlerForURLScheme('http', bundle_id)
-    LSSetDefaultHandlerForURLScheme('https', bundle_id)
-    LSSetDefaultHandlerForURLScheme('ftp', bundle_id)
-    #LSSetDefaultRoleHandlerForContentType('public.html', KLSRolesAll, bundle_id)
-    #LSSetDefaultRoleHandlerForContentType(KUTTypeHTML, KLSRolesViewer, bundle_id)
-    #LSSetDefaultRoleHandlerForContentType(KUTTypeURL, KLSRolesViewer, bundle_id)
-  end
-
   def handleGetURLEvent(event, withReplyEvent: replyEvent)
-    alert([:handleGetURLEvent, event].inspect) if MY_DEBUG
     urlStr = event.paramDescriptorForKeyword(KeyDirectObject).stringValue
-    setURL(urlStr)
+    @mainWindowController.setURL(urlStr)
 
     app = NSRunningApplication.currentApplication
     app.activateWithOptions(NSApplicationActivateIgnoringOtherApps)
-  end
-
-  def setURL(urlStr)
-    @url = urlStr
-    alert([:setURL, @url].inspect) if MY_DEBUG
-    @text.stringValue = @url if @text
-  end
-
-  def openBrowser(app_bundle_id)
-    url = NSURL.URLWithString @text.stringValue
-    ret = NSWorkspace.sharedWorkspace.openURLs([url], withAppBundleIdentifier: app_bundle_id, options: NSWorkspaceLaunchDefault, additionalEventParamDescriptor: nil, launchIdentifiers: nil)
-    unless ret
-      alert "Failed to open #{app}"
-    end
-  end
-
-  def alert(message)
-    alert = NSAlert.new
-    alert.messageText = message
-    alert.runModal
-  end
-
-  def openFirefox
-    openBrowser('org.mozilla.Firefox')
-  end
-
-  def openChrome
-    openBrowser('com.google.Chrome')
-  end
-
-  def openSafari
-    openBrowser('com.apple.Safari')
   end
 end
--- app/main_window.rb
+++ app/main_window.rb
@@ -1,5 +1,46 @@
 class MainWindow < NSWindowController
   extend IB
 
-  ib_outlet :text, NSTextField
+  ib_outlet :text, NSTextView
+
+  def setURL(urlStr)
+    @url = urlStr
+    @text.stringValue = @url if @text
+  end
+
+  def openBrowser(app_bundle_id)
+    url = NSURL.URLWithString @text.stringValue
+    ret = NSWorkspace.sharedWorkspace.openURLs([url], withAppBundleIdentifier: app_bundle_id, options: NSWorkspaceLaunchDefault, additionalEventParamDescriptor: nil, launchIdentifiers: nil)
+    unless ret
+      alert "Failed to open #{app}"
+    end
+  end
+
+  def alert(message)
+    alert = NSAlert.new
+    alert.messageText = message
+    alert.runModal
+  end
+
+  def openFirefox(sender)
+    openBrowser('org.mozilla.Firefox')
+  end
+
+  def openGoogleChrome(sender)
+    openBrowser('com.google.Chrome')
+  end
+
+  def openSafari(sender)
+    openBrowser('com.apple.Safari')
+  end
+
+  def setDefaultBrowser(sender)
+    bundle_id = NSBundle.mainBundle.bundleIdentifier
+    LSSetDefaultHandlerForURLScheme('http', bundle_id)
+    LSSetDefaultHandlerForURLScheme('https', bundle_id)
+    LSSetDefaultHandlerForURLScheme('ftp', bundle_id)
+    #LSSetDefaultRoleHandlerForContentType('public.html', KLSRolesAll, bundle_id)
+    #LSSetDefaultRoleHandlerForContentType(KUTTypeHTML, KLSRolesViewer, bundle_id)
+    #LSSetDefaultRoleHandlerForContentType(KUTTypeURL, KLSRolesViewer, bundle_id)
+  end
 end
